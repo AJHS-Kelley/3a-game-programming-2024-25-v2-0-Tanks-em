fc33969643ac2dcc266b07111a2b8b4d98676556
Tanks-em <s11900115@students.duvalschools.org>
2024-09-06 12:59:04 -0400

    while loops

--- 00_computer_science_exercises/02_flow_control_structures/looooooops.py
+++ 00_computer_science_exercises/02_flow_control_structures/looooooops.py
@@ -1,4 +1,6 @@
 # loops, Conner Oquinn, v0.0
+import random # import the random module for us to use
+# generally put all your import statements at the top
 
 # two types of loops
 #for --- used when you know how many loops you'll need
@@ -30,6 +32,25 @@ for eachFruit in fruits:
     print(eachFruit)
 
 # for loops using range(). range(x) is exclusive it starts at 0 and ends at x - 1
-    for 1 in range(10): # range is 0 - 0
-        print(1)
-        
+    for i in range(10): # range is 0 - 0
+        print(i)
+    
+# might not always want to start at zero
+for i in range(10, 100): #
+    print(i)
+
+# not want to count by 1
+for i in range(10, 100, 5): # 10 = start - 1 = stop, 5 = # to count by
+    print(i)
+
+# sometimes you're not done writing the loops
+    for x in range(10):
+        pass # tells python this loops isn't finished, don't freak out
+
+# while loops -- musical chairs
+playerScore = 0
+while playerScore < 100 # run as long as this is true
+print(f"Starting: {playerScore}")
+playerScore += random.radiant(1,3)
+print(f"After: {playerScore}")
+
