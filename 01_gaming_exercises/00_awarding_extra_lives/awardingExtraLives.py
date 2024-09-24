bcad3dbcf5b87272d2d284240389f6dad48088ff
Tanks-em <s11900115@students.duvalschools.org>
2024-09-24 13:03:22 -0400

    awarding extra lives finished

--- 01_gaming_exercises/00_awarding_extra_lives/awardingExtraLives.py
+++ 01_gaming_exercises/00_awarding_extra_lives/awardingExtraLives.py
@@ -3,7 +3,7 @@
 lives = 3
 
 # allow the user to input the score as an integer
-score = # Look line 18 from the user_input.py file for an example.  
+playerScore = int(input("Please type an integer number and press enter.\n")) # Look line 18 from the user_input.py file for an example.  
 # if score is 10000 or less
     # lose a life
 # if score is > 10000 but less than 100001
@@ -14,13 +14,13 @@ score = # Look line 18 from the user_input.py file for an example.
 # output the score and number of lives to the screen
 
 
-if score <= 10000:
+if playerScore <= 10000:
     print("loseALife.\n")
     # change the number of lives. 
-elif score < 100001:
+elif playerScore < 100001:
     print("oneExtraLife.\n")
     # change the number of lives. 
 else:
     print("twoExtraLives.\n")
     # change the number of lives. 
-print(f"Your score is {score}! You gained {life} Lives")
+print(f"Your score is {playerScore}! You gained {lives} Lives")
